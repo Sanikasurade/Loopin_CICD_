386cf447a056e3b040727e8eafa7e23461b2f8f7
Sanika <sanikasurade2021@gmail.com>
2025-12-15 18:18:10 +0530

    fixed app,jsx for browser

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,108 +1,81 @@
-import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { io } from "socket.io-client";
+
 import SignUp from "./pages/SignUp.jsx";
 import SignIn from "./pages/SignIn.jsx";
-import ForgotPassword from "./pages/forgotpassword.jsx";
+import ForgotPassword from "./pages/ForgotPassword.jsx";
 import Home from "./pages/Home.jsx";
-import getCurrentUser from "./hooks/getCurrentUser.jsx";
-import { useDispatch, useSelector } from "react-redux";
-import getSuggestedUsers from "./hooks/getSuggestedUsers.jsx";
 import Profile from "./pages/Profile.jsx";
 import EditProfile from "./pages/EditProfile.jsx";
 import Upload from "./pages/Upload.jsx";
-import getAllPost from "./hooks/getAllPost.jsx";
 import Messages from "./pages/Messages.jsx";
 import MessageArea from "./pages/MessageArea.jsx";
-import React,{ useEffect } from "react";
-import {io} from "socket.io-client"
-import { setOnlineUsers, setSocket } from "./redux/socketSlice.js";
+
+import getCurrentUser from "./hooks/getCurrentUser.jsx";
+import getSuggestedUsers from "./hooks/getSuggestedUsers.jsx";
+import getAllPost from "./hooks/getAllPost.jsx";
 import getFollowingList from "./hooks/getFollowingList.jsx";
 import getPrevChatUsers from "./hooks/getPrevchatUsers.jsx";
-import { BrowserRouter,Navigate,Route,Routes } from "react-router-dom";
 
-export const serverUrl=""
+import { setOnlineUsers, setSocket } from "./redux/socketSlice.js";
+
+// ✅ Backend URL
+export const serverUrl = "http://localhost:5000"; // change in production
+
 function App() {
+  const dispatch = useDispatch();
+  const { userData } = useSelector((state) => state.user);
+  const { socket } = useSelector((state) => state.socket);
+
+  // ✅ Load initial data
   getCurrentUser();
   getSuggestedUsers();
   getAllPost();
   getFollowingList();
   getPrevChatUsers();
-  // getAllLoops();
-  const {userData}=useSelector(state=>state.user)
-    const {socket}=useSelector(state=>state.socket)
-    const dispatch=useDispatch();
-  useEffect(()=>{
-    if(userData){
-      const socketIo=io({
-        query:{
-          userId:userData._id
-        }
-      })
-      dispatch(setSocket(socketIo))
-      socketIo.on('getOnlineUsers',(users)=>{
-        dispatch(setOnlineUsers(users))
-        console.log("Online Users:",users);
-      })
-      return ()=>socketIo.close()
 
-    }else{
-      if(socket){
-        socket.close()
-        dispatch(setSocket(null))
-     
+  // ✅ Socket connection
+  useEffect(() => {
+    if (userData) {
+      const socketIo = io(serverUrl, {
+        query: { userId: userData._id },
+        transports: ["websocket"],
+      });
+
+      dispatch(setSocket(socketIo));
+
+      socketIo.on("getOnlineUsers", (users) => {
+        dispatch(setOnlineUsers(users));
+        console.log("Online Users:", users);
+      });
+
+      return () => socketIo.close();
+    } else {
+      if (socket) {
+        socket.close();
+        dispatch(setSocket(null));
       }
     }
-  },[userData])
- 
+  }, [userData]);
+
   return (
     <BrowserRouter>
-    <Routes>
-      <Route
-        path="/signup"
-        element={!userData ? <SignUp /> : <Navigate to={"/"} />}
-      />
-      <Route
-        path="/signin"
-        element={!userData ? <SignIn /> : <Navigate to={"/"} />}
-      />
-      <Route
-        path="/"
-        element={userData ? <Home /> : <Navigate to={"/signin"} />}
-      />
-      <Route
-        path="/forgotpassword"
-        element={!userData ? <ForgotPassword /> : <Navigate to={"/"} />}
-      />
-      <Route
-        path="/profile/:userName"
-        element={userData ? <Profile /> : <Navigate to={"/signin"} />}
-      />
-      <Route
-        path="/editprofile"
-        element={userData ? <EditProfile /> : <Navigate to={"/signin"} />}
-      />
-      <Route
-        path="/upload"
-        element={userData ? <Upload /> : <Navigate to={"/signin"} />}
-      />
-      {/* <Route
-        path="/loops"
-        element={userData ? <Loops /> : <Navigate to={"/signin"} />}
-      /> */}
-      <Route
-        path="/messages"
-        element={userData ? <Messages/> : <Navigate to={"/signin"} />}
-      />
-       <Route
-        path="/messageArea"
-        element={userData ? <MessageArea/> : <Navigate to={"/signin"} />}
-      />
-      
+      <Routes>
+        <Route path="/signup" element={!userData ? <SignUp /> : <Navigate to="/" />} />
+        <Route path="/signin" element={!userData ? <SignIn /> : <Navigate to="/" />} />
+        <Route path="/forgotpassword" element={!userData ? <ForgotPassword /> : <Navigate to="/" />} />
 
-
-    </Routes>
+        <Route path="/" element={userData ? <Home /> : <Navigate to="/signin" />} />
+        <Route path="/profile/:userName" element={userData ? <Profile /> : <Navigate to="/signin" />} />
+        <Route path="/editprofile" element={userData ? <EditProfile /> : <Navigate to="/signin" />} />
+        <Route path="/upload" element={userData ? <Upload /> : <Navigate to="/signin" />} />
+        <Route path="/messages" element={userData ? <Messages /> : <Navigate to="/signin" />} />
+        <Route path="/messageArea" element={userData ? <MessageArea /> : <Navigate to="/signin" />} />
+      </Routes>
     </BrowserRouter>
   );
 }
-export default App;
 
+export default App;
